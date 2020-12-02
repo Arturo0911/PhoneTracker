1a20267aac2ad535f8ae4975a6251be709ed1a47
Arturon <anegreiross@outlook.com>
2020-12-02 18:24:25 -0500

    added TODO

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,16 +6,45 @@ import java.net.*;
 public class Main {
 
     private static final String url = "localhost";
+    private static final String ErrorMessage = "NO SERVIVCE!";
+
 
     /*TODO:
     * 1.- find which protocols are running on the cellphones ?
     * 2.- How can i get information about that/those protocols ?
+    *  Learn how the system of comunications like FDMA and CDMA (There are for
+    * cellphones comunications.
+    *
+    * 
+    *
+    *
+    *
+    * How a cellphone work ?
+    *
+    * The cellphone use a protocols of comunications arre
+    *
+    *
     *
     *
     * */
 
     public static void main(String[] args) {
-        System.out.println("Initializaing PhoneTracker");
+
+        InetAddress ip ;
+
+
+        try {
+            ip = InetAddress.getLocalHost();
+
+            System.out.println("Initializaing PhoneTracker");
+
+            System.out.println(ip);
+            System.out.println(ip.getHostName());
+        }catch (Exception e ){
+            System.out.println(e.toString());
+        }
+
+        //fabiodionisio
 
         // first create socket connection
 
