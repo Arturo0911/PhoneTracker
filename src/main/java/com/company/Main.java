f32c15334bd5bc56311ed3cf452716f7050fe210
Arturon <anegreiross@outlook.com>
2020-12-02 16:46:45 -0500

    first commit

---
+++ src/main/java/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+
+import java.net.*;
+
+public class Main {
+
+    private static final String url = "localhost";
+
+    /*TODO:
+    * 1.- find which protocols are running on the cellphones ?
+    * 2.- How can i get information about that/those protocols ?
+    *
+    *
+    * */
+
+    public static void main(String[] args) {
+        System.out.println("Initializaing PhoneTracker");
+
+        // first create socket connection
+
+    }
+}
