4422a11de09a1e7fcf3a12e793ec56a62ea936e3
Arturon <anegreiross@outlook.com>
2020-12-02 23:53:33 -0500

    added new information

--- src/main/java/com/company/Cellphone.java
+++ src/main/java/com/company/Cellphone.java
@@ -8,12 +8,26 @@ public class Cellphone {
      *   devices and used to mobile communications
      *
      * How a cellphone work ?
+     *
+     * FDMA frequency division multiple access are multiplexación technique to comunication from multiples protocols
+     * like comunication type digital and analogic
+     *
+     * In comunications the multiplexation are the technique of mix one or more signals comunications and transmitting
+     * over a single transmission medium
+     *
+     * Every cellphone has identifiers like
+     * IMEI(INTERNATIONAL MOBILE EQUIPMENT  IDENTIFIER => 15 DIGITS),
+     * EQUIPMENT SERIAL NUMBER (ESN => IS IN TWO FORMS DECIMAL AND HEXADECIMAL)
+     * MEID (MOBILE EQUIPMENT IDENTIFIER GLOBALLY UNIQUE NUMBER)
+     * MSN (MOBILE SERIAL NUMBER => 10 DIGIT AND IS REQUIRED FOR ANY SIM CARD)
+     * PIN (PERSONAL IDENTIFIEL NUMBER USING IN THE BLACKBERRY PHONES)
+     *
     */
 
     public Cellphone(){}
 
     public void stablishComunication(){
-        
+
     }
 
 }
